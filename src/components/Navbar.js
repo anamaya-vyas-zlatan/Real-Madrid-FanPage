6556efac41de7080be84d9fa3df6bf8820b1a8d4
unknown <anamayav@gmail.com>
2023-06-26 01:36:33 +0530

    check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 import {Stack } from '@mui/material'
 
 const Navbar = () => {
   return (
     <div className= 'first-name'style={{top: '160px', left:'120px', }}>
       <Stack direction ="column">
-      <a href =  "/" style={{color: 'white', fontSize: '20px'}} >Home</a>
+      <a href =  "/" style={{color: 'white', fontSize: '20px'}} >Calender</a>
       <a href =  "/" style={{color: 'white', fontSize: '20px'}} >Home</a>
       <a href =  "/" style={{color: 'white', fontSize: '20px'}} >Home</a>
       </Stack>
