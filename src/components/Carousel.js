61e276234c801709b00b1dac3e6b16b2b94c6fec
unknown <anamayav@gmail.com>
2023-06-27 04:19:54 +0530

    Updated trophies

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,43 +1,7 @@
-import { useState } from "react";
 import Slider from "react-slick";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-import { trophiesList } from '../constants/trophiesFile'
-import x from '../assets/logo.png'
 import CarouselCard from "./CarouselCard";
 
-function Carousel() {
-
-
-  const NextArrow = ({ onClick }) => {
-    return (
-      <div className="arrow next" onClick={onClick}>
-        <FaArrowRight />
-      </div>
-    );
-  };
-
-  const PrevArrow = ({ onClick }) => {
-    return (
-      <div className="arrow prev" onClick={onClick}>
-        <FaArrowLeft />
-      </div>
-    );
-  };
-
-  const [imageIndex, setImageIndex] = useState(0);
-  const [image, setImage] = useState(trophiesList[0].id)
-
-  const settings = {
-    infinite: true,
-    lazyLoad: true,
-    speed: 300,
-    slidesToShow: 3,
-    centerMode: true,
-    centerPadding: 0,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-    beforeChange: (current, next) => setImageIndex(next), 
-  };
+function Carousel({imageIndex, settings, trophiesList}) {
 
   return (
     <>
@@ -45,17 +9,12 @@ function Carousel() {
         <Slider {...settings}>
           {trophiesList.map((img, idx) => (
             <div className={idx === imageIndex ? "slide activeSlide" : "slide"}>
-            {/* <div> */}
-              {/* <img src={img.logo} alt={img.id} /> */}
               <CarouselCard logo = {img.logo}  name = {img.id} winner = {img.won}/>
-              {/* { console.log(imageIndex)} */}
-
             </div>
           ))}
 
         </Slider>
       </div>
-      {/* <CarouselCard/> */}
     </>
   );
 }
--- src/pages/Trophies.js
+++ src/pages/Trophies.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Carousel from '../components/Carousel'
+import { trophiesList } from '../constants/trophiesFile'
+import NextArrow from '../components/NextArrow';
+import PrevArrow from '../components/PrevArrow';
 
 const Trophies = () => {
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next), 
+  };
+
   return (
     <div>
-      <Carousel />
+      <Carousel imageIndex = {imageIndex}  settings = {settings} trophiesList = {trophiesList} />
     </div>
   )
 }
